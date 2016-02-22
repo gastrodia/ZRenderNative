39597939d30ff27ceea594afc7400c862bc2bdab
xuyang <xuyang@wozlla.com>
2016-02-22 22:18:21 +0800

    open window

---
+++ ZRenderNative/MainGame.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <SDL\SDL.h>
+#include <GL\glew.h>
+class MainGame
+{
+public:
+	MainGame();
+	~MainGame();
+
+	void run();
+	void initSystems();
+
+private:
+	SDL_Window* _window;
+	int _screenWidth;
+	int _screenHeight;
+};
+
--- ZRenderNative/main.cpp
+++ ZRenderNative/main.cpp
@@ -1,6 +1,14 @@
-#include <SDL\SDL.h>
+#include <Windows.h>
+#include <iostream>
+
+#include "MainGame.h"
 
 int main(int argc,char** argv) {
-	SDL_Init(SDL_INIT_EVERYTHING);
+	MainGame mainGame;
+	mainGame.run();
+
+	std::cout << "Enter any key to quit..";
+	int a;
+	std::cin >> a;
 	return 0;
 }
