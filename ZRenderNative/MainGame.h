ddfed7ec8e9a7abed86787bbcfbc5d127b050d75
xuyang <xuyang@wozlla.com>
2016-02-22 23:33:37 +0800

    glContext

--- ZRenderNative/MainGame.h
+++ ZRenderNative/MainGame.h
@@ -1,10 +1,9 @@
 #pragma once
-#include <SDL\SDL.h>
-#include <GL\glew.h>
 
-enum class GameState {
-	PLAY, EXIT
-};
+#include <SDL/SDL.h>
+#include <GL/glew.h>
+
+enum class GameState { PLAY, EXIT };
 
 class MainGame
 {
@@ -16,15 +15,13 @@ public:
 
 private:
 	void initSystems();
-
 	void gameLoop();
-
 	void processInput();
+	void drawGame();
 
 	SDL_Window* _window;
 	int _screenWidth;
 	int _screenHeight;
-
 	GameState _gameState;
 };
 
